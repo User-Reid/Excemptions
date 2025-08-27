f0f21e7d5652493d1f680f0989004467ef9a8d73
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-26 22:57:15 -0500

    completed another assignment over throwing excemptions :)

--- Program.cs
+++ Program.cs
@@ -88,3 +88,23 @@
 //     YearOfBirth = yearOfBirth;
 //   }
 // }
+
+public static class ExceptionsRethrowing
+    {
+        public static int GetMaxValue(List<int> numbers)
+        {
+    try
+    {
+      return numbers.Max();
+    }
+    catch(ArgumentNullException ex)
+    {
+      throw new ArgumentNullException("The numbers list cannot be null.", ex);
+    }
+    catch(InvalidOperationException)
+    {
+      System.Console.WriteLine("The numbers list cannot be empty.");
+      throw;
+          }
+        }
+    }
