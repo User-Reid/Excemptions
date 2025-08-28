996a603d14ebb7bbd019ea589eff770ced1e3ad7
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-28 14:31:31 -0500

    understanding custom exceptions.

--- OtherExamples/ExceptionFilters.cs
+++ OtherExamples/ExceptionFilters.cs
@@ -1,35 +1,11 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace Excemptions.Numbers
 {
   public class Examples2
   {
-    void Tacobell()
+    void Example1()
     {
-      try
-      {
-
-        var dataFromWeb = SendHttpRequest("www.someAddress.com/get/someResource");
-      }
-      catch (HttpRequestException ex) when (ex.Message == "403")
-      {
-        System.Console.WriteLine("It was forbidden to access the resouce.");
-        throw;
-      }
-      catch (HttpRequestException ex) when (ex.Message == "404")
-      {
-        System.Console.WriteLine("The resouce was not found.");
-        throw;
-      }
-      catch (HttpRequestException ex) when (ex.Message.StartsWith("4"))
-      {
-        System.Console.WriteLine("There was some kind of client error.");
-        throw;
-      }
-      catch (HttpRequestException ex) when (ex.Message == "500")
-      {
-        System.Console.WriteLine("There was an interal issue homie.");
-      }
-
-      Console.ReadKey();
-    }
+         }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,88 +1,132 @@
-﻿using Excemptions.Numbers;
+﻿// using Excemptions.Numbers;
 
-// var invallidPersonObject = new Person("", -100);
+// // var invallidPersonObject = new Person("", -100);
 
-// var emptyCollection = new List<int>();
-// var firstElement = GetFirstElement(emptyCollection);
+// // var emptyCollection = new List<int>();
+// // var firstElement = GetFirstElement(emptyCollection);
 
-// var numbers = new int[] { 1, 2, 3 };
-// var fourth = numbers[3];
-// var firstUsingLinq = emptyCollection.First();
+// // var numbers = new int[] { 1, 2, 3 };
+// // var fourth = numbers[3];
+// // var firstUsingLinq = emptyCollection.First();
 
-// bool has7 = CheckIfContains(7, numbers);
+// // bool has7 = CheckIfContains(7, numbers);
 
-// bool CheckIfContains(int value, int[] numbers)
+// // bool CheckIfContains(int value, int[] numbers)
+// // {
+// //   throw new NotImplementedException();
+// // }
+
+// try
 // {
-//   throw new NotImplementedException();
+//   var result = IsFirstElementPositive(null);
+// }
+// catch (NullReferenceException ex)
+// {
+
 // }
 
-try
-{
-  var result = IsFirstElementPositive(null);
-}
-catch (NullReferenceException ex)
-{
+// Console.ReadKey();
 
-}
+// void RecursiveMethod()
+// {
+//   System.Console.WriteLine("I'm going to call myself");
+//   RecursiveMethod();
+// }
+
+// int GetFirstElement(IEnumerable<int> numbers)
+// {
+//   foreach (var number in numbers)
+//   {
+//     return number;
+//   }
+//   throw new InvalidOperationException("The collection cant be empty shit for brains💩");
+// }
+
+// bool IsFirstElementPositive(IEnumerable<int> numbers)
+// {
+//   try
+//   {
+//     var firstElement = GetFirstElement(numbers);
+//     return firstElement > 0;
+//   }
+//   catch (InvalidOperationException ex)
+//   {
+//     System.Console.WriteLine("The collection is empty!");
+//     return true;
+//   }
+//   catch (NullReferenceException ex)
+//   {
+//     System.Console.WriteLine("Sorry homie! The application experience\r\n an unexpected error.");
+//     throw;
+//     // throw new ArgumentNullException("The collection is null.", ex);
+//   }
+// }
+
+// class Person
+// {
+//   public string Name { get; }
+//   public int YearOfBirth { get; }
+
+//   public Person(string name, int yearOfBirth)
+//   {
+//     if (name is null)
+//     {
+//       throw new ArgumentNullException("The name cannot be null jackass🤡.");
+//     }
+//     if (name == string.Empty)
+//     {
+//       throw new ArgumentException("The name cannot be empty jackass🤡.");
+//     }
+//     if (yearOfBirth < 1990 || yearOfBirth > DateTime.Now.Year)
+//     {
+//       throw new ArgumentOutOfRangeException("The year of birth must be\r\n between 1900 and the current year jackass🤡.");
+//     }
+
+//     Name = name;
+//     YearOfBirth = yearOfBirth;
+//   }
+// }
+
+using System.Reflection.Metadata;
+using System.Runtime.Serialization;
+
+throw new Exception();
 
 Console.ReadKey();
 
-void RecursiveMethod()
-{
-  System.Console.WriteLine("I'm going to call myself");
-  RecursiveMethod();
-}
+[Serializable]
 
-int GetFirstElement(IEnumerable<int> numbers)
+public class CustomException : Exception
 {
-  foreach (var number in numbers)
-  {
-    return number;
-  }
-  throw new InvalidOperationException("The collection cant be empty shit for brains💩");
-}
+  public int StatusCode { get; }
 
-bool IsFirstElementPositive(IEnumerable<int> numbers)
-{
-  try
+  protected CustomException(SerializationInfo info, StreamingContext context) : base(info, context);
+  public CustomException()
   {
-    var firstElement = GetFirstElement(numbers);
-    return firstElement > 0;
+
   }
-  catch (InvalidOperationException ex)
+
+  public CustomException(string message, int statusCode) : base(message)
   {
-    System.Console.WriteLine("The collection is empty!");
-    return true;
+    StatusCode = statusCode;
   }
-  catch (NullReferenceException ex)
+
+  public CustomException(string message, int statusCode, Exception innerException) : base(message, innerException)
   {
-    System.Console.WriteLine("Sorry homie! The application experience\r\n an unexpected error.");
-    throw;
-    // throw new ArgumentNullException("The collection is null.", ex);
+    StatusCode = statusCode;
+  }
+
+  public CustomException(string message) : base(message)
+  {
+
   }
 }
 
-class Person
+try
 {
-  public string Name { get; }
-  public int YearOfBirth { get; }
+  BlobHandle blah blah
+}
+catch(CustomException("Taco Bell", 404) ex)
+{
 
-  public Person(string name, int yearOfBirth)
-  {
-    if (name is null)
-    {
-      throw new ArgumentNullException("The name cannot be null jackass🤡.");
-    }
-    if (name == string.Empty)
-    {
-      throw new ArgumentException("The name cannot be empty jackass🤡.");
-    }
-    if (yearOfBirth < 1990 || yearOfBirth > DateTime.Now.Year)
-    {
-      throw new ArgumentOutOfRangeException("The year of birth must be\r\n between 1900 and the current year jackass🤡.");
-    }
-
-    Name = name;
-    YearOfBirth = yearOfBirth;
-  }
 }
